4cf54241b63cb9286df8e32f43026e545952bb61
Cash <cashwugeek@icloud.com>
2025-08-20 14:39:06 +0800

    Day 30 - ollama embedding

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,18 @@
 package com.cashwu
 
 import ai.koog.embeddings.local.LLMEmbedder
-import ai.koog.prompt.executor.clients.openai.OpenAILLMClient
-import ai.koog.prompt.executor.clients.openai.OpenAIModels
+import ai.koog.embeddings.local.OllamaEmbeddingModels
+import ai.koog.prompt.executor.ollama.client.OllamaClient
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 suspend fun main() {
 
     // 建立 OpenAI 用戶端
-    val client = OpenAILLMClient(ApiKeyManager.openAIApiKey!!)
+    val client = OllamaClient()
 
-    // 建立嵌入器，使用 TextEmbeddingAda002 模型
-    val embedder = LLMEmbedder(client, OpenAIModels.Embeddings.TextEmbedding3Small)
+    // 建立嵌入器，使用 NOMIC_EMBED_TEXT 模型
+    val embedder = LLMEmbedder(client, OllamaEmbeddingModels.NOMIC_EMBED_TEXT)
 
     // 產生文字嵌入
     val text = "Kotlin 是一個現代的程式語言"
